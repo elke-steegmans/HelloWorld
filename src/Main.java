27f331651e9d9441c59accb70340243c23a872c3
Elke Steegmans <Elke.Steegmans@khleuven.be>
2018-05-25 11:42:32 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Hello World!");
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ src/PersonTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class PersonTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test () {
+        System.out.println("ELKE");
+    }
+}
