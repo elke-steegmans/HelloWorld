b306f5c11bc2b82eee9f133aae74154f4fe9601c
Elke Steegmans <Elke.Steegmans@khleuven.be>
2018-05-25 13:32:02 +0200

    ELKE removed

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
         JOptionPane.showMessageDialog(null, "Hello World!");
         System.out.println("Hello World!");
-        System.out.println("ELKESSSSSSS");
-
 
     }
 }
