29a4233dbe68f14838e972c91ca064c1e6976f85
Greetje Jongen <greetje.jongen@ucll.be>
2018-06-05 11:51:46 +0200

    nb 3

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         JOptionPane.showMessageDialog(null, "Hello World!");
         System.out.println("Hello World!");
-        System.out.println("Hello World nb 2!");
+        System.out.println("Hello World nb 3!");
         
 
     }
